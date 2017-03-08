f91adf3071ef9eb55b0f6891b1cdc285fac3230d
Ingu <ingukk@gmail.com>
2017-03-08 21:19:31 +0100

    hire ceo as employee on save

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,12 @@
 class Company < ApplicationRecord
-  belongs_to :ceo, foreign_key: 'ceo_id', optional: true, :class_name => 'Employee'
+  belongs_to :ceo, foreign_key: 'ceo_id', optional: true, :class_name => 'Employee', autosave: true, counter_cache: true
   has_and_belongs_to_many :employees
   validates :name, presence: true
+  before_save :hireCeo
+
+  def hireCeo
+    hireEmployee(ceo) if ceo.present?
+  end
 
   def fireEmployee(employee)
     return unless employee.present?
@@ -12,7 +17,7 @@ class Company < ApplicationRecord
 
   def hireEmployee(employee)
     return unless employee.present?
-    self.employees << employee
+    self.employees << employee unless employees.include?(employee)
     self.save
   end
 end
